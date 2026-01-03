4906f87fe35701f3a1a5d5ea905a122effaa3698
Benbow <benrocks2005@gmail.com>
2026-01-02 20:25:52 -0500

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#EFEFEF] font-sans text-black">
       <Link href="/gettingstarted">
-      <Button>HELLO I AM REALEST BOY FOR TESTING</Button>
+        <Button>HELLO I AM REALEST BOY FOR TESTING</Button>
       </Link>
     </div>
   );
